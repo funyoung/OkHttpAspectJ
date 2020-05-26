88740be2eb81e1e095023bd598e785cbb293bb5d
yangfeng <yangfeng@sogou-inc.com>
2020-05-26 17:51:35 +0800

    add ime switcher.

--- app/src/main/java/com/journaldev/okhttp/MainActivity.java
+++ app/src/main/java/com/journaldev/okhttp/MainActivity.java
@@ -1,10 +1,18 @@
 package com.journaldev.okhttp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,6 +20,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -23,10 +34,11 @@ import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView txtString;
+    private Button asynchronousGet, synchronousGet, asynchronousPOST;
 
-
-    TextView txtString;
-    Button asynchronousGet, synchronousGet, asynchronousPOST;
+    private Button switchButton;
 
     public String url = "https://reqres.in/api/users/2";
     public String postUrl = "https://reqres.in/api/users/";
@@ -47,9 +59,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         synchronousGet = findViewById(R.id.synchronousGet);
         asynchronousPOST = findViewById(R.id.asynchronousPost);
 
+        switchButton = findViewById(R.id.ime_switcher);
+
         asynchronousGet.setOnClickListener(this);
         synchronousGet.setOnClickListener(this);
         asynchronousPOST.setOnClickListener(this);
+
+        switchButton.setOnClickListener(this);
 
         txtString = findViewById(R.id.txtString);
     }
@@ -137,9 +153,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     e.printStackTrace();
                 }
                 break;
+            case R.id.ime_switcher:
+                if (isstop || 0 == startStamp) {
+                    ((InputMethodManager)MainActivity.this.getSystemService(Context.INPUT_METHOD_SERVICE)).showInputMethodPicker();
+                } else {
+                    stopWatch();
+                }
+                break;
 
         }
     }
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        startWatch();
+//    }
 
     public class OkHttpHandler extends AsyncTask<String, Void, String> {
 
@@ -167,6 +196,105 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             txtString.setText(s);
         }
     }
+
+    private final HashMap<String, List<Long>> costTrackerMap = new HashMap<>();
+    private long startStamp;
+    private long stopStamp;
+    private boolean isstop = false;
+    private final long REFRESH_INTERVAL = 100;
+    private final int REFRESH_CODE = 1;
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            // TODO Auto-generated method stub
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case REFRESH_CODE:
+                    updateView();
+                    break;
+                case 0:
+                    break;
+            }
+        }
+
+    };
+
+    private void startWatch() {
+        startStamp = stopStamp = System.currentTimeMillis();
+        isstop = false;
+        updateView();
+    }
+
+    private void stopWatch() {
+        stopStamp = System.currentTimeMillis();
+        isstop = true;
+        updateView();
+    }
+
+    private void nextTimer() {
+        mHandler.sendEmptyMessageDelayed(REFRESH_CODE, REFRESH_INTERVAL);
+    }
+
+    // 添加更新ui的代码
+    private void updateView() {
+        long difference = (System.currentTimeMillis() - startStamp);
+        if (isstop) {
+            appendTrack();
+            String displayText = String.format("切换输入法(%d'%d\")", difference / 1000, difference % 1000);
+            switchButton.setText(displayText);
+        } else {
+            nextTimer();
+            String displayText = String.format("%d'%d\"", difference / 1000, difference % 1000);
+            switchButton.setText(displayText);
+        }
+    }
+
+    private void appendTrack() {
+        String ss= Settings.Secure.getString(getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD);
+        final List<Long> ssTrackList;
+        if (costTrackerMap.containsKey(ss)) {
+            ssTrackList = costTrackerMap.get(ss);
+        } else {
+            ssTrackList = new ArrayList<>();
+            costTrackerMap.put(ss, ssTrackList);
+        }
+        ssTrackList.add(stopStamp - startStamp);
+        Log.i(TAG, costTrackerMap.toString());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listenToImeChanged();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopListening();
+    }
+
+    private static final String INPUT_METHOD_ACTION = "android.intent.action.INPUT_METHOD_CHANGED";
+
+    private void listenToImeChanged() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(INPUT_METHOD_ACTION);
+        registerReceiver(inputMethodChangeReceiver, intentFilter);
+    }
+
+    private void stopListening() {
+        unregisterReceiver(inputMethodChangeReceiver);
+    }
+
+    private BroadcastReceiver inputMethodChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(INPUT_METHOD_ACTION)) {
+                //监听到输入法发生改变
+                startWatch();
+            }
+        }
+    };
 }
 
 
